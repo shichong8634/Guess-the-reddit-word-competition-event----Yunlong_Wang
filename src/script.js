dc8eab7140c05f722d27c77e6d842fb843b3855b
shichong8634 <2057646043@qq.com>
2026-06-21 22:03:42 +0800

    Adding a script, which is used to navigate to another page when the user selects an option from a dropdown menu: 1. Get the dropdown element. 2. Detect when the selected option changes. 3. Read the selected value (URL). 4. Redirect the browser to that page.
    ----Yunlong Wang(Computing major(Year 2) in Griffith College Dublin)
    ----Updated in 22:03 21/6/2026

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const selectBox = document.getElementById("selectBox");
+
+selectBox.onchange = function () {
+    const url = this.value;
+    if(url){
+        window,location.href = url;
+    }
+}
