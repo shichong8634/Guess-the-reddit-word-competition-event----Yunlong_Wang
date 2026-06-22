98f9467b8bd688358d2d22404c10d9339816c64b
shichong8634 <2057646043@qq.com>
2026-06-22 13:20:34 +0800

    Making a simple word guessing mini-game. It stores 5 fixed internet slang words and randomly picks one as the hidden answer. Players type guesses in an input box and submit them. It counts guess attempts (max 6 tries). It shows different prompts: correct message if matching, "Wrong Guess" for mistakes, or game over text revealing the answer after 6 failed tries.
    ----Yunlong Wang(Computing major(Year 2) in Griffith College Dublin)
    ----Updated in 13:20 22/6/2026

--- src/script.js
+++ src/script.js
@@ -3,6 +3,55 @@ const selectBox = document.getElementById("selectBox");
 selectBox.onchange = function () {
     const url = this.value;
     if(url){
-        window,location.href = url;
+        window.location.href = url;
     }
+}
+
+const words = [
+    "KARMA",
+    "MEME",
+    "UPVOTE",
+    "MOD",
+    "COMMENT"
+]
+
+const answer = words[Math.floor(Math.random() * words.length)];
+
+let attempts = 0;
+
+function checkGuess() {
+    let guess =
+        document.getElementById("guessInput")
+            .value
+            .toUpperCase();
+
+    attempts++;
+
+    document.getElementById("attempts")
+        .innerHTML =
+        "Attempts: " + attempts + "/6";
+
+    if (guess === answer) {
+
+        document.getElementById("result")
+            .innerHTML =
+            "💓🤗🤗🤗💓 Correct!";
+
+    }
+
+    else if (attempts >= 6) {
+
+        document.getElementById("result")
+            .innerHTML =
+            "💔🙄🙄🙄💔 Game Over! The answer was: " + answer;
+
+    }
+
+    else {
+
+        document.getElementById("result")
+            .innerHTML =
+            "Wrong Guess!";
+    }
+
 }
